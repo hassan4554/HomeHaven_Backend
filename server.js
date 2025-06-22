4ddbe013a08296638a88009362358180d1930642
hassan4554 <hm9536057@gmail.com>
2025-06-22 21:55:31 +0500

    added cors

--- server.js
+++ server.js
@@ -10,11 +10,7 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: "https://home-haven-frontend-ruby.vercel.app/",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", routes);
