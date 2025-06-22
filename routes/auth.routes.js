e14a3d151c33bb8b43b237d448f0052d675f0f6d
hassan4554 <hm9536057@gmail.com>
2025-06-22 16:06:47 +0500

    resolved problem

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { validateData } from "../middlewares/validation.js";
-import { signupSchema } from "../Schema/index.js";
+import { signupSchema } from "../schema/index.js";
 import { signin, signup, google, signout } from "../controllers/auth/index.js";
 const router = express.Router();
 
